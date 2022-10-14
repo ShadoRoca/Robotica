e30aafe053f9dd9af02b9a85947d8a6abc0d2654
Tu Nombre <godites@ciencias.unam.mx>
2022-10-14 14:32:54 -0500

    cambios ros2

--- Cuadrado_ws/src/Cuadrado/setup.py
+++ Cuadrado_ws/src/Cuadrado/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        	'Cuadrado = Cuadrado.cuadrado_prueba:main'
+        	'Cuadrado = Cuadrado.cuadrado:main'
         ],
     },
 )
